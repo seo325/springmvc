30f65eb9c1bd86ca02e112f6bd785f503e703429
seohwa0325 <seohwa0325@gmail.com>
2023-10-08 19:30:48 +0900

    Initial commit

---
+++ src/main/java/hello/springmvc/basic/requestmappeing/MappingController.java
@@ -0,0 +1,32 @@
+package hello.springmvc.basic.requestmappeing;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class MappingController {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+
+    @RequestMapping("/hello-basic")
+    public String hello(){
+        log.info("helloBasic");
+        return "ok";
+    }
+    @RequestMapping(value = "/mapping-get-v1", method = RequestMethod.GET)
+    public String mappingGetV1() {
+        log.info("mappingGetV1");
+        return "ok";
+    }
+    @GetMapping(value = "/mapping-get-v2")
+    public String mappingGetV2() {
+        log.info("mapping-get-v2");
+        return "ok";
+    }
+}
