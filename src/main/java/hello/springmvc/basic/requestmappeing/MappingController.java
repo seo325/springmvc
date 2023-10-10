46c9292e47b6d81065f9c600a4bfba54e256f578
seohwa0325 <seohwa0325@gmail.com>
2023-10-10 17:39:38 +0900

    MappingController

--- src/main/java/hello/springmvc/basic/requestmappeing/MappingController.java
+++ src/main/java/hello/springmvc/basic/requestmappeing/MappingController.java
@@ -2,10 +2,8 @@ package hello.springmvc.basic.requestmappeing;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 
@@ -15,18 +13,50 @@ public class MappingController {
 
 
     @RequestMapping("/hello-basic")
-    public String hello(){
+    public String hello() {
         log.info("helloBasic");
         return "ok";
     }
+
     @RequestMapping(value = "/mapping-get-v1", method = RequestMethod.GET)
     public String mappingGetV1() {
         log.info("mappingGetV1");
         return "ok";
     }
+
     @GetMapping(value = "/mapping-get-v2")
     public String mappingGetV2() {
         log.info("mapping-get-v2");
+        return "ok";
+    }
+
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String data) {
+        log.info("mappingPath userId={}", data);
+        return "ok";
+    }
+
+    @GetMapping(value = "/mapping-param", params = "mode-debug")
+    public String mappingParam(){
+        log.info("mappingParam");
+        return "ok";
+    }
+
+    @GetMapping(value = "/mapping-header", headers = "mode=debug")
+    public String mappingHeader() {
+        log.info("mappingHeader");
+        return "ok";
+    }
+
+    @PostMapping(value = "/mapping-consume", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String mappingConsumes() {
+        log.info("mappingConsumes");
+        return "ok";
+    }
+
+    @PostMapping(value = "/mapping-produce", produces = MediaType.TEXT_HTML_VALUE)
+    public String mappingProduces() {
+        log.info("mappingProduces");
         return "ok";
     }
 }
